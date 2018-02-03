bc35a281ce9e95ded0a021f142b52779950070cd
juhwan <jukim92@gmail.com>
2018-02-03 17:37:20 -0500

    Test

--- app/src/main/java/com/example/yanfengjin/helloworld/MainActivity.java
+++ app/src/main/java/com/example/yanfengjin/helloworld/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.yanfengjin.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("MyApp", "Hello World");
+                Toast.makeText(getApplicationContext(),"hello world", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
-    //test
+
 }
