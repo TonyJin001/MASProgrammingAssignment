366863b753371f8c92b26a6a302ec2a11d6c4f1c
juhwan <jukim92@gmail.com>
2018-02-03 23:13:49 -0500

    Pull from database

--- app/src/main/java/com/example/yanfengjin/helloworld/MainActivity.java
+++ app/src/main/java/com/example/yanfengjin/helloworld/MainActivity.java
@@ -7,19 +7,25 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
+    DatabaseReference test1;
+    String message1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        test1 = FirebaseDatabase.getInstance().getReference("test1").child("Hello World");
         Button button = findViewById(R.id.btn);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.i("MyApp", "Hello World");
-                Toast.makeText(getApplicationContext(),"hello world", Toast.LENGTH_SHORT).show();
+                message1 = test1.getKey();
+                Log.i("MyApp", message1);
+                Toast.makeText(getApplicationContext(),message1, Toast.LENGTH_SHORT).show();
             }
         });
     }
